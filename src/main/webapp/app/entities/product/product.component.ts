a6d8d6acf1530e44f22a33f1883c56eeb5c1b6f0
soesandanwe <soesandanwe@gmail.com>
2019-05-16 16:54:21 +0800

    Update the product component view

--- src/main/webapp/app/entities/product/product.component.ts
+++ src/main/webapp/app/entities/product/product.component.ts
@@ -29,6 +29,7 @@ export class ProductComponent implements OnInit, OnDestroy {
     predicate: any;
     previousPage: any;
     reverse: any;
+    filter: string;
 
     constructor(
         protected productService: ProductService,
