25529c4cc7fc4e23a7ff12fcc26cef3c041d901d
soesandanwe <soesandanwe@gmail.com>
2019-05-28 14:56:45 +0800

    Initial application generated by JHipster-5.8.2

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -31,50 +31,6 @@ import { RouterModule } from '@angular/router';
             {
                 path: 'shipment',
                 loadChildren: './shipment/shipment.module#StoreShipmentModule'
-            },
-            {
-                path: 'product',
-                loadChildren: './product/product.module#StoreProductModule'
-            },
-            {
-                path: 'customer',
-                loadChildren: './customer/customer.module#StoreCustomerModule'
-            },
-            {
-                path: 'product-order',
-                loadChildren: './product-order/product-order.module#StoreProductOrderModule'
-            },
-            {
-                path: 'order-item',
-                loadChildren: './order-item/order-item.module#StoreOrderItemModule'
-            },
-            {
-                path: 'invoice',
-                loadChildren: './invoice/invoice.module#StoreInvoiceModule'
-            },
-            {
-                path: 'shipment',
-                loadChildren: './shipment/shipment.module#StoreShipmentModule'
-            },
-            {
-                path: 'customer',
-                loadChildren: './customer/customer.module#StoreCustomerModule'
-            },
-            {
-                path: 'product-order',
-                loadChildren: './product-order/product-order.module#StoreProductOrderModule'
-            },
-            {
-                path: 'order-item',
-                loadChildren: './order-item/order-item.module#StoreOrderItemModule'
-            },
-            {
-                path: 'invoice',
-                loadChildren: './invoice/invoice.module#StoreInvoiceModule'
-            },
-            {
-                path: 'shipment',
-                loadChildren: './shipment/shipment.module#StoreShipmentModule'
             }
             /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
         ])
