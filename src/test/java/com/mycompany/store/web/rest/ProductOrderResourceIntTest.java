0af09350cfa27238dc7f3abbef5f0c56b4b03664
soesandanwe <soesandanwe@gmail.com>
2019-06-04 17:02:49 +0800

    Initial application generated by JHipster-5.8.2

--- src/test/java/com/mycompany/store/web/rest/ProductOrderResourceIntTest.java
+++ src/test/java/com/mycompany/store/web/rest/ProductOrderResourceIntTest.java
@@ -13,12 +13,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -45,7 +43,6 @@ import com.mycompany.store.domain.enumeration.OrderStatus;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = StoreApp.class)
-@WithMockUser(username="admin", authorities={"ROLE_ADMIN"}, password = "admin")
 public class ProductOrderResourceIntTest {
 
     private static final Instant DEFAULT_PLACED_DATE = Instant.ofEpochMilli(0L);
@@ -76,7 +73,6 @@ public class ProductOrderResourceIntTest {
     private EntityManager em;
 
     @Autowired
-    @Qualifier("defaultValidator")
     private Validator validator;
 
     private MockMvc restProductOrderMockMvc;
